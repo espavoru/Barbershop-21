3665ecb3e3328801194f99fb865abd72af9377f5
Oleg <espavoru@gmail.com>
2020-12-29 17:58:49 +0200

    Add Advantages slider

---
+++ source/sass/blocks/slider/slider.js
@@ -0,0 +1,29 @@
+const slider = (selector) => {
+  const slider = document.querySelector(`${selector}`);
+  const slides = slider.querySelectorAll(".slider__item");
+  const toggles = slider.querySelectorAll(".slider__toggle");
+
+  const clickHandler = (e) => {
+    if (e.target.closest(".slider__toggle")) {
+      toggles.forEach((toggle) => {
+        toggle.classList.remove("slider__toggle--active");
+      });
+
+      e.target.classList.add("slider__toggle--active");
+
+      slides.forEach((slide) => {
+        slide.classList.remove("slider__item--active");
+      });
+
+      toggles.forEach((toggle, i) => {
+        if (toggle.classList.contains("slider__toggle--active")) {
+          slides[i].classList.add("slider__item--active");
+        }
+      });
+    }
+  };
+
+  slider.addEventListener("click", clickHandler);
+};
+
+slider(".advantages");
